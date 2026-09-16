0d8fcb87a1372863e02646c3aacf86375eb17622
Dionclei-Pereira <dionclei2@gmail.com>
2026-09-16 11:49:06 -0300

    feat(file): add file reader and string buffer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello world";
+    if (argc < 2) {
+        std::cout << "Usage: nebula <file.nb>\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if (!file) {
+        std::cerr << "Error: could not open the file";
+        return 1;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+
+    std::string code = buffer.str();
+    std::cout << code;
+    return 0;
 }
